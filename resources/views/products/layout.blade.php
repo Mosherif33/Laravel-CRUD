321af2f478f1e770758e2762faf9a488ad730af5
Mohamed Sherif <s.shecoo@gmail.com>
2022-11-20 13:00:48 +0200

    first commit

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel 8 CRUD App</title>
+        <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css
+        ">
+    </head>
+    <body>
+        <h1 align="center">Laravel 9 CRUD App</h1>
+
+        <div class="container">
+
+            <br>
+            @yield('content')
+        </div>
+    </body>
+</html>
